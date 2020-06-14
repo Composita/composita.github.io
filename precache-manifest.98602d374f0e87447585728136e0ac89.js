9012da4c84ffeebc197a63279613f7d49f169d77
Darnor <Darnor@users.noreply.github.com>
2020-06-14 11:43:56 +0000

    deploy: f9b6d2d80f39aec90ae951c8b2346a16c662baa3

---
+++ precache-manifest.98602d374f0e87447585728136e0ac89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2aee73df9255616e6e1902d8cbf45bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9ffcbbcbb97f1c7ef7",
+    "url": "/static/css/2.cc930a2b.chunk.css"
+  },
+  {
+    "revision": "58fe915439ffbdec0c40",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf9ffcbbcbb97f1c7ef7",
+    "url": "/static/js/2.48d11441.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.48d11441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58fe915439ffbdec0c40",
+    "url": "/static/js/main.875f49fa.chunk.js"
+  },
+  {
+    "revision": "b2b1567ffd37def14392",
+    "url": "/static/js/runtime-main.378897fa.js"
+  }
+]);
