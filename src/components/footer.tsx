89a1dcafa07076e76c453846e4360f904f391586
Hansruedi Patzen <hp@revy.ch>
2022-01-03 21:51:27 +0100

    Update license year

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,12 @@ import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 
 class Footer extends Component<RouteComponentProps> {
     render(): JSX.Element {
+        const year = new Date().getFullYear();
         return (
             <div className="footer">
                 <footer className="footer mt-auto py-3 bg-light">
                     <p className="text-center">
-                        Copyright &copy; 2020 Hansruedi Patzen, licensed under <Link to="/license">0BSD</Link>.
+                        Copyright &copy; 2020-{year} Hansruedi Patzen, licensed under <Link to="/license">0BSD</Link>.
                     </p>
                 </footer>
             </div>
